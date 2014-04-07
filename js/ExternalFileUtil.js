1154d0b1f15b8b953dfd2fb9c42b49b8c8d61355
rohwinu <rohwinu@live.com>
2014-04-07 11:01:26 -0500

    C1

---
+++ js/ExternalFileUtil.js
@@ -0,0 +1,6 @@
+window.ExternalFileUtil = {
+    
+    openWith: function ( path, uti, success, fail) {
+        return cordova.exec(success, fail, "ExternalFileUtil", "openWith", [path, uti]);
+    }  
+};
